828389627236da6c0bcf87012b37ef73b03e3564
00752-tech <passiveincomestreaming@gmail.com>
2025-01-17 17:53:39 -0800

    Update route.ts

--- app/api/webinar/route.ts
+++ app/api/webinar/route.ts
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 
 export async function GET() {
-  redirect('https://c092dm095hi7goi4ai989o5tas.hop.clickbank.net/?&traffic_source=2025')
+  redirect('https://c092dm095hi7goi4ai989o5tas.hop.clickbank.net/?cbpage=jcwebinar')
 }
 
