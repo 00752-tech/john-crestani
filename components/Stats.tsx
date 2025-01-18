c08b7db9e2aa4e87bd8ed3fd299ecf2e7f2c68b4
00752-tech <passiveincomestreaming@gmail.com>
2025-01-18 11:28:33 -0800

    Update Stats.tsx

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -13,6 +13,7 @@ export default function Stats() {
     { number: 10836, label: "Active Students", suffix: "+" },
     { number: 8, label: "of Proven Results", suffix: "+ Years" },
     { number: 24, label: "Support", suffix: "/7" },
+    { number: 4.6, label: "Rated on Trustpilot", suffix: " Stars" },
   ]
 
   return (
@@ -47,4 +48,3 @@ export default function Stats() {
     </section>
   )
 }
-
