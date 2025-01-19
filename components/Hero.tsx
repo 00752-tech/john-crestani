badc968de5f92ad48492931cbb3c268edd36782c
00752-tech <passiveincomestreaming@gmail.com>
2025-01-18 17:33:25 -0800

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,9 +22,9 @@ export default function Hero() {
           viewport={{ once: true }}
           transition={{ duration: 0.8 }}
         >
-          <h1 className=" text-center sm:text-left text-3xl sm:text-4xl md:text-6xl font-bold mb-4 text-white">
-            Innovate, Disrupt, Prosper in 2025: The Crestani Affiliate Advantage
-          </h1>
+          <h1 className="text-center sm:text-left text-3xl sm:text-4xl md:text-6xl font-light mb-4 text-white">
+  Innovate, Disrupt, Prosper in 2025: The Crestani Affiliate Advantage</h1>
+
           <h2 className="text-2xl md:text-3xl font-semibold mb-6 gradient-text">
            {` John Crestani's Super Affiliate System Pro`}
           </h2>
