4e889e9ad91272154b5a65c8b0ffa8678c7126de
00752-tech <passiveincomestreaming@gmail.com>
2025-01-18 19:09:51 -0800

    Update CTA.tsx

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -69,7 +69,7 @@ export default function CTA() {
             transition={{ duration: 0.3 }}
             className="bg-gradient-to-r from-pink-500 to-purple-500 text-white py-4 px-12 rounded-full text-xl font-semibold hover:from-pink-600 hover:to-purple-600 transition duration-300"
           >
-            Ignite Success with Crestani&apos;s Training
+            Buy Now & Start Earning
           </motion.button>
           <motion.p 
             initial={{ opacity: 0 }}
