e3e76c89bf4146906fb3dbd45ab86830e1475ae2
00752-tech <passiveincomestreaming@gmail.com>
2025-01-18 18:03:06 -0800

    Update route.ts

--- app/api/sale/route.ts
+++ app/api/sale/route.ts
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 
 export async function GET() {
-  redirect('https://0d7acj-7slidlmj33db09v5m7x.hop.clickbank.net/?cbpage=quizreg')
+  redirect('https://fca39lr7vdf3ip5g4h8-cr6v66.hop.clickbank.net/?cbpage=dirsp1')
 }
 
