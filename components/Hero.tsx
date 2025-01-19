ba7d216182dc6fea6df9ea32d3bb8da715366269
00752-tech <passiveincomestreaming@gmail.com>
2025-01-19 12:06:58 -0800

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -32,7 +32,7 @@ export default function Hero() {
           </div>
           
           <h1 style="text-align: left; font-size: 2.8rem; font-weight: medium; margin-bottom: 1.625rem; line-height: 1.625; color: white;">
-  Break through the affiliate income barrier with Crestani's training
+  Break through the affiliate income barrier with Crestani&apos; training
 </h1>
           <h2 className="text-2xl md:text-1xl font-semibold mb-6 gradient-text">
             {` John Crestani's Super Affiliate System Pro`}
