7a2965a22fdb0b37075369b26b3634faf4d1bbd4
00752-tech <passiveincomestreaming@gmail.com>
2025-01-19 09:39:54 -0800

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,12 +22,12 @@ export default function Hero() {
           viewport={{ once: true }}
           transition={{ duration: 0.8 }}
         >
-          <div className="text-center sm:text-left text-sm sm:text-base uppercase text-white mb-2 tracking-wider">
-          Struggling to Convert Clicks into Cash?
+          <div className="text-center sm:text-left text-xl sm:text-2xl uppercase text-white mb-4 tracking-wider">
+            Struggling to Convert Clicks into Cash?
           </div>
-
-          <h1 className="text-center sm:text-left text-2xl sm:text-3xl md:text-[2.8rem] font-light mb-4 text-white leading-tight">
-          Break through the <b>affiliate income</b> barrier with Crestani&apos;s training
+          <h1 className="text-center sm:text-left text-2xl sm:text-3xl md:text-[2.8rem] font-light mb-6 text-white leading-relaxed">
+            Break through the <b>affiliate income</b> <br className="hidden sm:block" />
+            barrier with Crestani&apos;s training
           </h1>
           <h2 className="text-2xl md:text-3xl font-semibold mb-6 gradient-text">
             {` John Crestani's Super Affiliate System Pro`}
