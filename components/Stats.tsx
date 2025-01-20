2848a448519e2210b5c7d39c82382ab84034de53
00752-tech <passiveincomestreaming@gmail.com>
2025-01-20 15:24:53 -0800

    Update Stats.tsx

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-import { motion, useMotionValue, useTransform, animate } from 'framer-motion'
-import { useEffect } from 'react'
+import { motion } from 'framer-motion'
 import CountUp from 'react-countup'
 
 export default function Stats() {
