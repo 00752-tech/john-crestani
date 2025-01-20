28c7aa6685c6246f67609480b59d16a579ede5ad
00752-tech <passiveincomestreaming@gmail.com>
2025-01-20 10:13:04 -0800

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,8 +27,10 @@ export default function Hero() {
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 text-gray-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M12 22c6.23-.05 7.87-5.6 7.4-8.5h-3.4c-.38 0-.62-.23-.63-.62V9.49c-.04-2.2-1.56-4.2-3.4-5.12C10.66 3.84 9.16 2.84 8 2c0 0-1.03 2.11-.13 4.26C5.47 7.19 4 9.44 4 12c0 5.48 4.62 10 8 10z" />
               </svg>
-              <span className="text-lg text-gray-500 text-left">AWARD-WINNING EDUCATOR IN AFFILIATE MARKETING</span>
-            </div>
+              <span className="text-lg text-left font-semibold bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 text-transparent bg-clip-text">
+  AWARD-WINNING EDUCATOR IN AFFILIATE MARKETING
+</span>
+
           </div>
           
           <h1 className="text-left text-2xl md:text-[2.8rem] font-medium mb-4 leading-tight text-white">
