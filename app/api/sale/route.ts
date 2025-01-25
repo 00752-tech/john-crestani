6e073e6f968d0f363a319f756188f300168ab907
00752-tech <passiveincomestreaming@gmail.com>
2025-01-24 16:29:14 -0800

    Update route.ts

--- app/api/sale/route.ts
+++ app/api/sale/route.ts
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 
 export async function GET() {
-  redirect('https://fca39lr7vdf3ip5g4h8-cr6v66.hop.clickbank.net/?cbpage=dirsp1')
+  redirect('https://f22b0lo80heesrcdgfa9es5l52.hop.clickbank.net/?cbpage=diroop&campaign=order_limited_discount')
 }
 
