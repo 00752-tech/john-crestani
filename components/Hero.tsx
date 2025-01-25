1af090491d8907bb67feb3bf6d708eaba8f87355
00752-tech <passiveincomestreaming@gmail.com>
2025-01-25 12:51:00 -0800

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -59,7 +59,7 @@ export default function Hero() {
                 <path d="M12 22c6.23-.05 7.87-5.6 7.4-8.5h-3.4c-.38 0-.62-.23-.63-.62V9.49c-.04-2.2-1.56-4.2-3.4-5.12C10.66 3.84 9.16 2.84 8 2c0 0-1.03 2.11-.13 4.26C5.47 7.19 4 9.44 4 12c0 5.48 4.62 10 8 10z" />
               </svg>
               <span className="text-lg text-left font-semibold bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 text-transparent bg-clip-text">
-                GLOBAL RECOGNITION: AWARD FOR OUTSTANDING CONTRIBUTION TO AFFILIATE MARKETING EDUCATION
+                GFEL AWARD RECIPIENT: TRANSFORMING AFFILIATE MARKETING EDUCATION
               </span>
             </div>
           </div>
