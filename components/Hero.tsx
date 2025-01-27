97d1048c9be67d4d60bb4bb752970ed66b53f119
00752-tech <passiveincomestreaming@gmail.com>
2025-01-26 18:09:02 -0800

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -94,25 +94,23 @@ const Hero: React.FC = () => {
           transition={{ duration: 0.8 }}
         >
           <div className="flex flex-col items-start mb-4">
-            <div className="flex items-center text-2xl text-gray-500 mb-2">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 text-gray-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <div className="flex items-center text-2xl mb-2">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 text-blue-300" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M12 22c6.23-.05 7.87-5.6 7.4-8.5h-3.4c-.38 0-.62-.23-.63-.62V9.49c-.04-2.2-1.56-4.2-3.4-5.12C10.66 3.84 9.16 2.84 8 2c0 0-1.03 2.11-.13 4.26C5.47 7.19 4 9.44 4 12c0 5.48 4.62 10 8 10z" />
               </svg>
-              <span className="text-lg text-left font-semibold bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 text-transparent bg-clip-text">
+              <span className="text-lg text-left font-semibold text-blue-300">
                 GLOBAL RECOGNITION: JOHN CRESTANI RECEIVES TOP HONORS IN EDUCATION
               </span>
             </div>
           </div>
           
-          <h1 className="text-left text-3xl md:text-[2.8rem] font-normal mb-6 leading-tight">
-            <span className="bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 text-transparent bg-clip-text">
-              Stuck in a rut? John Crestani&apos;s training can change your life </span> 
+          <h1 className="text-left text-3xl md:text-[2.8rem] font-normal mb-6 leading-tight text-white">
+            Stuck in a rut? John Crestani&apos;s training can 
+            <span className="text-blue-400"> change your life </span> 
           </h1>
           
-          <p className="text-base text-left sm:text-xl mb-8 leading-relaxed">
-            <span className="bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 text-transparent bg-clip-text">
-              Exhausted by constant setbacks? Find success with expert training.
-            </span>
+          <p className="text-base text-left sm:text-xl mb-8 leading-relaxed text-gray-300">
+            Exhausted by constant setbacks? Find success with expert training.
           </p>
           
           <motion.div 
