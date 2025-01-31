51fa8b97e9b36d5e226e1ac8a6beacb3182a37ba
00752-tech <passiveincomestreaming@gmail.com>
2025-01-31 07:35:31 -0800

    Update WebinarInvite.tsx

--- components/WebinarInvite.tsx
+++ components/WebinarInvite.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { motion } from "framer-motion";
+
 export default function WebinarInvite() {
   const handleWebinarClick = () => {
-    window.location.href = "/api/webinar";
+    window.location.href = "https://eb304ktx6ll4qhe38o6bep4s36.hop.clickbank.net/?&campaign=webi2";
   };
 
   return (
@@ -22,7 +23,8 @@ export default function WebinarInvite() {
         >
           Exclusive Webinar Invite
         </motion.h2>
-        <motion.div  initial={{ opacity: 0, x: 80 }}
+        <motion.div 
+          initial={{ opacity: 0, x: 80 }}
           whileInView={{
             opacity: 1,
             x: 0,
@@ -30,7 +32,9 @@ export default function WebinarInvite() {
           transition={{
             duration: 0.5,
             ease: "easeInOut",
-          }}  className="max-w-5xl mx-auto">
+          }}  
+          className="max-w-5xl mx-auto"
+        >
           <div className="relative w-full pb-[56.25%]">
             <iframe
               src="https://drive.google.com/file/d/1zOZ9PwE89-JoUOUpjiGzrnt0siRpXUsV/preview"
@@ -38,21 +42,26 @@ export default function WebinarInvite() {
               className="absolute top-0 left-0 w-full h-full rounded-lg shadow-2xl"
             ></iframe>
           </div>
-          <motion.div   initial={{ opacity: 0, x: -80 }}
-          whileInView={{
-            opacity: 1,
-            x: 0,
-          }}
-          transition={{
-            duration: 0.5,
-            ease: "easeInOut",
-          }} className="mt-12 text-center">
-            <button
+          <motion.div   
+            initial={{ opacity: 0, x: -80 }}
+            whileInView={{
+              opacity: 1,
+              x: 0,
+            }}
+            transition={{
+              duration: 0.5,
+              ease: "easeInOut",
+            }} 
+            className="mt-12 text-center"
+          >
+            <a
+              href="https://eb304ktx6ll4qhe38o6bep4s36.hop.clickbank.net/?&campaign=webi2"
               onClick={handleWebinarClick}
               className="inline-block bg-gradient-to-r from-pink-500 to-purple-500 text-white py-4 px-12 rounded-full text-xl font-semibold hover:from-pink-600 hover:to-purple-600 transition duration-300 transform hover:scale-105 shadow-lg"
+              rel="sponsored"
             >
               Try it free
-            </button>
+            </a>
           </motion.div>
         </motion.div>
       </article>
