9af5eb3a07ca0b4dd36dc86d4ec86881f317343b
00752-tech <passiveincomestreaming@gmail.com>
2025-02-02 13:00:46 -0800

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -101,7 +101,7 @@ const Hero: React.FC = () => {
           </h1>
           
           <p className="text-sm sm:text-base md:text-lg lg:text-xl text-left mb-8 md:mb-12 leading-relaxed text-gray-300 font-light tracking-tight">
-          71% earn less than $1,000/month due to <span className="text-pink-500 font-semibold">outdated strategies</span> (42%) and <span className="text-emerald-400 font-semibold">lack of mentorship</span> (31%). Break free with John Crestani's <span className="text-yellow-400 font-semibold">proven, innovative approach</span>.
+          71% earn less than $1,000/month due to <span className="text-pink-500 font-semibold">outdated strategies</span> (42%) and <span className="text-emerald-400 font-semibold">lack of mentorship</span> (31%). Break free with John Crestani&apos;s <span className="text-yellow-400 font-semibold">proven, innovative approach</span>.
           </p>
 
 
