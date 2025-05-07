cf71271c1a4127b97156ff82b32f7311f3a8cb2a
00752-tech <passiveincomestreaming@gmail.com>
2025-05-07 11:04:05 -0700

    Update WebinarInvite.tsx

--- components/WebinarInvite.tsx
+++ components/WebinarInvite.tsx
@@ -39,8 +39,11 @@ export default function WebinarInvite() {
         >
           <div className="relative w-full pb-[56.25%]">
             <iframe
-              src="https://drive.google.com/file/d/1zOZ9PwE89-JoUOUpjiGzrnt0siRpXUsV/preview"
-              allow="autoplay"
+              src="https://www.youtube.com/embed/BBmO1xpQwQk?si=CPG5MnqPUGXTQf8f"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
               className="absolute top-0 left-0 w-full h-full rounded-lg shadow-2xl"
             ></iframe>
           </div>
