5d80dc8d110b82c1e9edb8391cadeb2c919fb2c1
00752-tech <passiveincomestreaming@gmail.com>
2025-06-30 15:20:31 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,16 +60,20 @@ export default function RootLayout({
         <Script id="ms-clarity" strategy="afterInteractive">
           {`
             (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
             })(window, document, "clarity", "script", "s6qlp9x2uo");
           `}
         </Script>
       </head>
-      <GoogleTagManager gtmId="G-R5SXJEQF8Z" />
-      <CookieConsent />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* Google Tag Manager */}
+        <GoogleTagManager gtmId="G-R5SXJEQF8Z" />
+        {/* Cookie Consent */}
+        <CookieConsent />
+        {children}
+      </body>
     </html>
   );
 }
