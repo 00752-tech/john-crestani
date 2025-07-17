68f6444fb990ad86bad4d49c0d6002cf50d9f432
00752-tech <passiveincomestreaming@gmail.com>
2025-07-17 13:12:05 -0700

    Create SkepticismSection.tsx

---
+++ components/SkepticismSection.tsx
@@ -0,0 +1,12 @@
+import WebinarInvite from '../components/WebinarInvite'
+import SkepticismSection from '../components/SkepticismSection'
+
+export default function HomePage() {
+  return (
+    <>
+      <WebinarInvite />
+      <SkepticismSection />
+      {/* ...other sections... */}
+    </>
+  )
+}
