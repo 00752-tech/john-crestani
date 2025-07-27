1abc3781015e5eb2c18736084bc4768915c3f17a
00752-tech <passiveincomestreaming@gmail.com>
2025-07-27 16:35:21 -0700

    Create metadata.ts

---
+++ app/advertising-tools/roas-calculator/metadata.ts
@@ -0,0 +1,5 @@
+export const metadata = {
+  title: 'Predictable Profits: AI-Powered ROAS Calculator',
+  description:
+    'Eliminate the guesswork from your ad spend. Our free AI-powered ROAS calculator helps you predict and optimize ad campaign performance for stable, profitable returns and confident marketing decisions.',
+};
