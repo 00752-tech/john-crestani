91ec03e77512d2fa03cd12cf71b9c5a73c73f72d
00752-tech <passiveincomestreaming@gmail.com>
2025-07-27 17:46:34 -0700

    Update page.tsx

--- app/content-creation-tools/youtube-earnings-calculator/page.tsx
+++ app/content-creation-tools/youtube-earnings-calculator/page.tsx
@@ -1,12 +1,12 @@
-import ClientYouTubeCalculator from "./ClientYouTubeCalculator";
-import { JsonLd } from "react-schemaorg";
+import ClientYouTubeCalculator from './ClientYouTubeCalculator'
+import { JsonLd } from 'react-schemaorg'
 
 export const metadata = {
-  title: "Predict Your YouTube Income: Free AdSense Earnings Calculator",
+  title: 'Predict Your YouTube Income: Free AdSense Earnings Calculator',
   description:
     "Are you tired of guessing your YouTube income? Our free YouTube AdSense Earnings Calculator removes uncertainty, giving you predictable insights to forecast your channel's monthly and yearly earnings and gain financial control.",
-};
+}
 
 export default function YouTubeCalculatorPage() {
-  return <ClientYouTubeCalculator />;
+  return <ClientYouTubeCalculator />
 }
