9c8a6ea9df032c87264f170c2dbd8a2632a6f0b2
00752-tech <passiveincomestreaming@gmail.com>
2025-07-29 07:07:45 -0700

    Update route.ts

--- app/api/sale/route.ts
+++ app/api/sale/route.ts
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 
 export async function GET() {
-  redirect('https://cebc9l43pa0y1p06ad69fldve3.hop.clickbank.net/?&traffic_source=sales_route')
+  redirect('https://johncrestani.me/super-affiliate-system-pro')
 }
 
