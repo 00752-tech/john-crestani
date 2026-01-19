6651cb8887db8ce4f1fe533f69052ea156ccf059
00752-tech <passiveincomestreaming@gmail.com>
2026-01-19 11:25:44 -0800

    Update RelatedTools.tsx

--- components/RelatedTools.tsx
+++ components/RelatedTools.tsx
@@ -1,6 +1,20 @@
+// components/RelatedTools.tsx
 'use client';
+interface RelatedToolsProps { /* empty or add props if needed */ }
+export default function RelatedTools({}: RelatedToolsProps) {
+  return <div>RelatedTools placeholder</div>;
+}
 
-export default function ROASCalculator() {
+// components/ROASCalculator.tsx  
+'use client';
+interface ROASCalculatorProps { /* add props if needed */ }
+export default function ROASCalculator({}: ROASCalculatorProps) {
   return <div>ROASCalculator placeholder</div>;
 }
 
+// components/AffiliateLinkCTROptimizer.tsx
+'use client';
+interface AffiliateLinkCTROptimizerProps { /* add props if needed */ }
+export default function AffiliateLinkCTROptimizer({}: AffiliateLinkCTROptimizerProps) {
+  return <div>AffiliateLinkCTROptimizer placeholder</div>;
+}
